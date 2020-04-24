8c750f3129d0024bad31cc76b09a371d1be94ec1
RostiMelk <hello@rosti.no>
2020-04-25 01:31:06 +0200

    Some progress on the copy code functionality

--- src/scripts/app.js
+++ src/scripts/app.js
@@ -7,7 +7,7 @@ ready(() => {
 	copyFieldName();
 	openDocs();
 	appendCopyCodeBtns(); 
-	// copyFieldCode();
+	copyFieldCode();
 	// appendFieldNameOnEdit();
 });
 
@@ -31,10 +31,12 @@ function copyFieldName() {
 			link.innerHTML = '<a href="#" class="copy-field-name" title="'+title+'">'+str+'</a>';
 		}
 	})
-	document.querySelector(".copy-field-name").addEventListener("click", (e) => {
-		e.preventDefault();
-		copyStringToClipboard(e.target);
-		copyMessage(chrome.i18n.getMessage('copiedFieldName'));
+	document.addEventListener('click', function(e) {
+		if (event.target.matches('.copy-field-name')) {
+			e.preventDefault();
+			copyStringToClipboard(e.target);
+			copyMessage(chrome.i18n.getMessage('copiedFieldName'));
+		}
 	});
 }
 
@@ -58,63 +60,65 @@ function openDocs() {
 Copy ACF meta field code feature:
 ----------------------*/
 function appendCopyCodeBtns() {
+	appendCopyCodeBtn();
+	appendCopyAllCodeBtn();
+	// Append buttons to new fields
+	document.addEventListener('click', function(e) {
+		if (event.target.matches('.add-field')) {
+			// Remove previously added buttons
+			btns = document.querySelectorAll(".copy-field-code");
+			btns.forEach(btn => {
+				btn.remove();
+			});
+			// Appends new ones
+			setTimeout(() => {
+				appendCopyCodeBtn();
+				appendCopyAllCodeBtn();
+			}, 10);
+		}
+	});
+	// Append a Copy code with sub fields button when changing a new field into a group or repeater
+	document.querySelector(".acf-field-setting-type .field-type").addEventListener("change", (e) => {
+		$(".copy-field-code.include-sub-fields").remove();
+		setTimeout(() => {
+			appendCopyAllCodeBtn();
+		}, 1000);
+	});
+
 	// Append copy field code button
 	function appendCopyCodeBtn() {
 		var btnStr = chrome.i18n.getMessage('copyCodeBtn'),
 			btnTitle = chrome.i18n.getMessage('copyCodeBtnTitle'),
 			btnHTML = '<a class="button button-primary button-small copy-field-code exclude-sub-fields" title="'+btnTitle+'" href="#">'+btnStr+'</a>',
-			container = document.querySelectorAll('.acf-field-object:not([data-type="accordion"]):not([data-type="message"]):not([data-type="tab"]) .row-options');
-		container.innerHTML = container.innerHTML + btnHTML;
+			row = document.querySelectorAll('.acf-field-object:not([data-type="accordion"]):not([data-type="message"]):not([data-type="tab"]) .row-options');
+		row.forEach(row => {
+			row.innerHTML += btnHTML;
+		});
 	}
-	appendCopyCodeBtn();
-
-	// Append buttons to new fields
-	document.querySelector(".add-field").addEventListener("click", (e) => {
-		document.querySelectorAll(".copy-field-code").remove();
-		setTimeout(() => {
-			appendCopyCodeBtn();
-		}, 10);
-	});
-
 	// Append copy all fields code button
 	function appendCopyAllCodeBtn() {
 		var btnStr = chrome.i18n.getMessage('copyAllCodeBtn'),
-			btnTitle = chrome.i18n.getMessage('copyAllCodeBtnTitle');
-		$('.acf-field-object[data-type="repeater"], .acf-field-object[data-type="group"]')
-			.children(".handle")
-			.find(".row-options")
-			.append('<a class="button button-primary button-small copy-field-code include-sub-fields" title="'+btnTitle+'" href="#">'+btnStr+'</a>');
+		btnTitle = chrome.i18n.getMessage('copyAllCodeBtnTitle'),
+		btnHTML = '<a class="button button-primary button-small copy-field-code include-sub-fields" title="'+btnTitle+'" href="#">'+btnStr+'</a>',
+		row = document.querySelectorAll('.acf-field-object[data-type="repeater"] > .handle .row-options, .acf-field-object[data-type="group"] > .handle .row-options');
+		row.forEach(row => {
+			row.innerHTML += btnHTML;
+		});
 	}
-	appendCopyAllCodeBtn();
-
-	// Append copy all fields code button new fields
-	$("body").on("click", ".add-field", function() {
-		$(".copy-field-code").remove();
-		setTimeout(function() {
-			appendCopyAllCodeBtn();
-		}, 10);
-	});
-	$("body").on("change", ".acf-field-setting-type .field-type", function() {
-		$(".copy-field-code.include-sub-fields").remove();
-		setTimeout(function() {
-			appendCopyAllCodeBtn();
-		}, 1000);
-	});
-}
-
-function getTypeOfField(thisField) {
-	// Get type of field
-	return thisField.closest(".acf-field-object").attr("data-type");
 }
 
 function getFieldName(thisField) {
 	// Get field name
 	return thisField
-		.closest(".handle")
-		.find(".li-field-name")
-		.text()
-		.trim();
+		.closest('.handle')
+
 }
+
+function getTypeOfField(thisField) {
+	// Get type of field
+	return thisField.closest(".acf-field-object").getAttribute("data-type");
+}
+
 
 function getReturnType(thisField, typeOfField) {
 	// Output type for supported fields (This is used if a field has multiple types of output methods, such as Array, ID or URL)
@@ -128,8 +132,8 @@ function getReturnType(thisField, typeOfField) {
 	) {
 		return thisField
 			.closest(".acf-field-object")
-			.find(".acf-field-setting-return_format li label.selected input")
-			.val();
+			.querySelectorAll(".acf-field-setting-return_format li label.selected input")
+			.value;
 	} else {
 		return null;
 	}
@@ -158,46 +162,49 @@ function getPlace() {
 
 function copyFieldCode() {
 	// Copy field code
-	$("body").on("click", ".copy-field-code", function() {
-		var thisField = $(this),
-		fieldName = getFieldName(thisField),
-		typeOfField = getTypeOfField(thisField),
-		returnType = getReturnType(thisField, typeOfField),
-		seniority = getSeniority(thisField),
-		place = getPlace(thisField),
-		subFields = "";
+	document.addEventListener('click', function(e) {
+		if (event.target.matches('.copy-field-code')) {
+			e.preventDefault();
+			var thisField = this,
+				fieldName = getFieldName(thisField),
+				typeOfField = getTypeOfField(thisField),
+				returnType = getReturnType(thisField, typeOfField),
+				seniority = getSeniority(thisField),
+				place = getPlace(thisField),
+				subFields = "";
 
-		// If user clicked copy code with sub fields. This will get the first level of child items.
-		if (thisField.hasClass("include-sub-fields")) {
-			thisField
-				.closest(".acf-field-object")
-				.children(".settings")
-				.children(".acf-table")
-				.children(".acf-field-settings")
-				.children(".acf-field-setting-sub_fields")
-				.children(".acf-input")
-				.children(".acf-field-list-wrap")
-				.children(".acf-field-list")
-				.children(".acf-field-object")
-				.children(".handle")
-				.find(".copy-field-code.exclude-sub-fields")
-				.each(function() {
-				var thisField = $(this),
-					fieldName = getFieldName(thisField),
-					typeOfField = getTypeOfField(thisField),
-					returnType = getReturnType(thisField, typeOfField),
-					seniority = getSeniority(thisField),
-					place = getPlace(thisField);
+			// If user clicked copy code with sub fields. This will get the first level of child items.
+			if (thisField.hasClass("include-sub-fields")) {
+				thisField
+					.closest(".acf-field-object")
+					.children(".settings")
+					.children(".acf-table")
+					.children(".acf-field-settings")
+					.children(".acf-field-setting-sub_fields")
+					.children(".acf-input")
+					.children(".acf-field-list-wrap")
+					.children(".acf-field-list")
+					.children(".acf-field-object")
+					.children(".handle")
+					.find(".copy-field-code.exclude-sub-fields")
+					.each(function() {
+					var thisField = $(this),
+						fieldName = getFieldName(thisField),
+						typeOfField = getTypeOfField(thisField),
+						returnType = getReturnType(thisField, typeOfField),
+						seniority = getSeniority(thisField),
+						place = getPlace(thisField);
 
-				acf_field(appendCode = true, fieldName, typeOfField, returnType, seniority, place, "");
+					acf_field(appendCode = true, fieldName, typeOfField, returnType, seniority, place, "");
 
-				subFields += sessionStorage.getItem("fieldcode");
-			});
+					subFields += sessionStorage.getItem("fieldcode");
+				});
+			}
+			acf_field(appendCode = false, fieldName, typeOfField, returnType, seniority, place, subFields);
+
+			// Clear session storage
+			sessionStorage.removeItem("fieldcode");
 		}
-		acf_field(appendCode = false, fieldName, typeOfField, returnType, seniority, place, subFields);
-
-		// Clear session storage
-		sessionStorage.removeItem("fieldcode");
 	});
 }
 
